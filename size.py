2da33dd18426ce2aba5a92278be1b4464d619e9a
Matt Schaub <mes100@zips.uakron.edu>
2019-02-04 23:53:22 -0500

    size function working appropriately

--- size.py
+++ size.py
@@ -1,11 +1,24 @@
 from el import*
 
-def size(e):
+def size(e): #compute the size of an expression
 	assert isinstance(e, Expr)
 
-	if type(e) is BoolExpr:
+	if isinstance(e, BoolExpr):
 		return 1
-	if type(e) is NotExpr: 
+
+	if isinstance(e, NotExpr): 
 		return 1 + size(e.expr)
+
 	if isinstance(e, BinaryExpr): 
 		return 1 + size(e.lhs) + size(e.rhs)
+
+def height(e): #compute the height of an expression
+	pass
+def same(e1, e2): #Return true if two expressions are identical
+	pass
+def value(e): #compute the value of an expression
+	pass
+def step(e): #Return an expression representing a single step of evaluation
+	pass
+def reduce(e):#Calls step repeatedly until the expression is non-reducible
+	pass
--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 from el import *
 from size import size
+
 e1 = AndExpr(NotExpr(BoolExpr(True)), 
 	BoolExpr(False))
+
+e2 = BoolExpr(True)
+
+print(size(e1))
